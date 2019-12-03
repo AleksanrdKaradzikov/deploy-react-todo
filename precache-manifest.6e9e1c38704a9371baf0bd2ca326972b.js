1d262cf362d7794d5d96facea835400f1fba222c
AleksandrKaradzikov <karadzikovaleksandr@gmail.com>
2019-12-03 23:21:00 +0300

    Updates

---
+++ precache-manifest.6e9e1c38704a9371baf0bd2ca326972b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f90443a5fc562d06beaa83921d0283",
+    "url": "/deploy-react-todo/index.html"
+  },
+  {
+    "revision": "4f8e7e064875162894ce",
+    "url": "/deploy-react-todo/static/css/main.e93ce778.chunk.css"
+  },
+  {
+    "revision": "a00c8383e79b0d7c4b5a",
+    "url": "/deploy-react-todo/static/js/2.9b44b8a5.chunk.js"
+  },
+  {
+    "revision": "4f8e7e064875162894ce",
+    "url": "/deploy-react-todo/static/js/main.ec2c0b83.chunk.js"
+  },
+  {
+    "revision": "9566c4b8c5c9738619b8",
+    "url": "/deploy-react-todo/static/js/runtime-main.440f0602.js"
+  }
+]);
